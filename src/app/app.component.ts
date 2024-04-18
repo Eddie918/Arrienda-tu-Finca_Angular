5ca1ede329661fa189cd2c41fc0ab43edea9ec0f
Jerson Osorio <61725276+Velorio@users.noreply.github.com>
2024-04-18 16:27:09 -0500

    commonModule agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./components/menu/menu.component";
 import { MainComponent } from "./components/main/main.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, MenuComponent, MainComponent, FooterComponent]
+    imports: [RouterOutlet, MenuComponent, MainComponent, FooterComponent, CommonModule]
 })
 export class AppComponent {
   title = 'AngularProyecto';
